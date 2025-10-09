44dbc25dc8b1a07ae3462ab424c70ba23a0c94fa
Emily Phanthavilay <emphanthavilay@ucsd.edu>
2025-10-09 14:33:20 -0700

    added src/snacks.ts to print an array of snacks

---
+++ src/snacks.ts
@@ -0,0 +1,17 @@
+class snack_array{
+    snacks: string[];
+
+    constructor(){
+        this.snacks = ['chips', 'juice', 'blackberries',
+            'ice cream'
+        ]
+    }
+}
+
+function print_name(list: snack_array){
+    console.log(list);
+}
+
+const snacks = new snack_array;
+
+print_name(snacks);
